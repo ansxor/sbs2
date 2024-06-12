59d4605d3e4895826319377b98053d6948fa8593
12Me21 <12Me21.mc@gmail.com>
2024-06-12 17:03:04 -0400

    new oboy ssh..

--- admin/update-oboy.sh
+++ admin/update-oboy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # yes but you don'[t know my PASSWORD!
 set -ue
-ssh ssh://12@oboy.smilebasicsource.com:240 -t 'cd ~/storage/sbs2/ && git pull --recurse-submodules && yes | ./admin/build-with-sourcemaps.sh ~/html/'
+ssh 12@oboy.smilebasicsource.com -t 'eval `ssh-agent` && ssh-add ~/.ssh/new-oboy && cd ~/storage/sbs2/ && git pull --recurse-submodules && yes | ./admin/build-with-sourcemaps.sh ~/html/'
 echo "done" >&2
